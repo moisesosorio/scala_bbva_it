8f0b6cf2bc5fbacc92d1707bb754714ff5d924af
moises.osorio@live.com <moises.osorio@live.com>
2023-05-19 21:26:17 +1000

    creating studentes tasks folder

--- src/main/scala/com/itformacion/scalacurso/ExecutionJob.scala
+++ src/main/scala/com/itformacion/scalacurso/ExecutionJob.scala
@@ -1,15 +1,24 @@
 package com.itformacion.scalacurso
 
+import com.typesafe.config.ConfigFactory
+import com.itformacion.scalacurso.process.{Basicos => ba}
+
 /**
  * @author ${user.name}
  */
-object ExecutionJob {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
+private object ExecutionJob {
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    val reference = args(0)
+    val globalConfig = ConfigFactory.load(reference)
+    val configIn = globalConfig.getConfig("ScalaCurso")
+    val ejecucionTema = configIn.getString("ejecucionTema")
+    ejecucionTema match {
+      case "1" => ba.InterpreteDeScala(configIn)
+      case "2" => ba.DeclarandoValoresVariables()
+      case "3" => ba.TiposDeUsoComun()
+      case "4" => ba.SobrecaraAritmeticaOperadores()
+      case "5" => ba.FuncionesMetodosDeLlamadas()
+      case "6" => ba.MetodoDeAplicacion()
+    }
   }
-
 }
--- src/main/scala/com/itformacion/scalacurso/process/Basicos.scala
+++ src/main/scala/com/itformacion/scalacurso/process/Basicos.scala
@@ -1,8 +1,41 @@
 package com.itformacion.scalacurso.process
 
+import com.typesafe.config.Config
 object Basicos {
 
-  def InterpreteDeScala(): Unit = {
+  def InterpreteDeScala(config: Config): Unit = {
+    /***
+     * Que es el interprete de Scala?
+     *
+     * Que hace o cual es su trabajo?
+     *
+     */
+
+
+    val integerSalida = 1 + 2
+    val stringSalida = "Hola Mundo"
+
+    val arrayFrutas = Array("manzana", "peras", "uvas")
+    val arrayVerduras = Array("tomate", "col", "coliflor")
+
+    val arrayTotal = arrayFrutas.++(arrayVerduras)
+
+    println(integerSalida)
+    println(stringSalida)
+    println(arrayFrutas)
+    println(arrayFrutas(2))
+    println(arrayTotal.mkString(","))
+
+    /**
+     * Ejercicio:
+     *
+     */
+
+
+    //    def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+//
+//    println("Hello World!")
+//    println("concat arguments = " + foo(args))
 
   }
 
