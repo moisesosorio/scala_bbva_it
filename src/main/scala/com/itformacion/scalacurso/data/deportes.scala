6b429d94a338d159a454352e284733812da6d3d6
E059232 <lydia.delgadods@bbvaitspain.com>
2023-05-22 13:20:17 +0200

    prueba

--- src/main/scala/com/itformacion/scalacurso/data/deportes.scala
+++ src/main/scala/com/itformacion/scalacurso/data/deportes.scala
@@ -1,5 +1,18 @@
 package com.itformacion.scalacurso.data
 
-class deportes {
+class Deportes (nombreVar: String, participantesVar: Int, seguidoresVar: Int) {
+  val nombre: String = nombreVar
+  val participantes: Int = participantesVar
+  val seguidores: Int = seguidoresVar
 
+  def &&(dep: Deportes): Deportes = {
+    new Deportes(nombre + " AND " + dep.nombre,
+      participantes + dep.participantes,
+      seguidores + dep.seguidores)
+  }
+  def ||(dep: Deportes): Deportes = {
+    new Deportes(nombre + " OR " + dep.nombre,
+      participantes + dep.participantes,
+      seguidores + dep.seguidores)
+  }
 }
